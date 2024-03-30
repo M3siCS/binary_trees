e66549d968ecfd3bb6f7c8b9fc9e8d9ab17c7ec7
M3siCS <hadeelkaddy86@gmail.com>
2024-03-29 20:11:33 -0700

    H

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,24 +6,24 @@
  * @second: A pointer to the second node
  *
  * Return: A pointer to the lowest common ancestor node,
- *         or NULL if no common ancestor was found
+ * or NULL if no common ancestor was found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-    const binary_tree_t *temp1 = first;
-    const binary_tree_t *temp2 = second;
+	const binary_tree_t *temp1 = first;
+	const binary_tree_t *temp2 = second;
 
-    while (temp1)
-    {
-        temp2 = second;
-        while (temp2)
-        {
-            if (temp1 == temp2)
-                return (binary_tree_t *)temp1;
-            temp2 = temp2->parent;
-        }
-        temp1 = temp1->parent;
-    }
-    return NULL;
+	while (temp1)
+	{
+		temp2 = second;
+		while (temp2)
+		{
+			if (temp1 == temp2)
+				return (binary_tree_t *)temp1;
+			temp2 = temp2->parent;
+		}
+		temp1 = temp1->parent;
+	}
+	return (NULL);
 }
 

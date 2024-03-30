5411ef4ece7773c7efea013cc37d707a0a75215a
root <arwamah777@gmail.com>
2024-03-29 20:41:00 -0700

    A

--- binary_trees.h
+++ binary_trees.h
@@ -51,11 +51,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
-<<<<<<< HEAD
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
-=======
 int binary_tree_is_complete(const binary_tree_t *tree);
->>>>>>> 2b855aef6c85aa366755069c5b13dba986d7b6a3
 
 
 #endif

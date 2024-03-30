d6688b468c69d0981415f8e3073d4388f403890f
M3siCS <hadeelkaddy86@gmail.com>
2024-03-29 19:17:35 -0700

    H

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,16 +5,16 @@
  * @tree: A pointer to the root node of the tree to count the nodes
  *
  * Return: The number of nodes with at least 1 child in the tree,
- *         or 0 if `tree` is NULL
+ *		or 0 if `tree` is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	if (tree->left != NULL || tree->right != NULL)
 		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 
-	return 0;
+	return (0);
 }
 

3f41f15076d679736246136160d0a37d1a1aa2f9
M3siCS <hadeelkaddy86@gmail.com>
2024-03-30 13:28:34 -0700

    H

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -9,9 +9,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node != NULL && node->left == NULL && node->right == NULL)
-        return (1);
-    return (0);
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
 }
 
 /**
@@ -22,15 +22,15 @@ int binary_tree_is_leaf(const binary_tree_t *node)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left, right;
+	size_t left, right;
 
-    if (tree == NULL)
-        return (0);
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
-    if (left >= right)
-        return (1 + left);
-    return (1 + right);
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
 }
 
 /**
@@ -41,22 +41,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    binary_tree_t *l, *r;
+	binary_tree_t *l, *r;
 
-    if (tree == NULL)
-        return (1);
-    l = tree->left;
-    r = tree->right;
-    if (binary_tree_is_leaf(tree))
-        return (1);
-    if (l == NULL || r == NULL)
-        return (0);
-    if (binary_tree_height(l) == binary_tree_height(r))
-    {
-        if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
-            return (1);
-    }
-    return (0);
+	if (tree == NULL)
+		return (1);
+	l = tree->left;
+	r = tree->right;
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	if (l == NULL || r == NULL)
+		return (0);
+	if (binary_tree_height(l) == binary_tree_height(r))
+	{
+		if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
+			return (1);
+	}
+	return (0);
 }
 
 /**
@@ -67,27 +67,27 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    size_t l_height, r_height;
-    binary_tree_t *l, *r;
+	size_t l_height, r_height;
+	binary_tree_t *l, *r;
 
-    if (tree == NULL)
-        return (0);
-    if (binary_tree_is_leaf(tree))
-        return (1);
-    l = tree->left;
-    r = tree->right;
-    l_height = binary_tree_height(l);
-    r_height = binary_tree_height(r);
-    if (l_height == r_height)
-    {
-        if (binary_tree_is_perfect(l) && binary_tree_is_complete(r))
-            return (1);
-    }
-    else if (l_height == r_height + 1)
-    {
-        if (binary_tree_is_complete(l) && binary_tree_is_perfect(r))
-            return (1);
-    }
-    return (0);
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	l = tree->left;
+	r = tree->right;
+	l_height = binary_tree_height(l);
+	r_height = binary_tree_height(r);
+	if (l_height == r_height)
+	{
+		if (binary_tree_is_perfect(l) && binary_tree_is_complete(r))
+			return (1);
+	}
+	else if (l_height == r_height + 1)
+	{
+		if (binary_tree_is_complete(l) && binary_tree_is_perfect(r))
+			return (1);
+	}
+	return (0);
 }
 
